480e9faf2f6e97665a6fcc791c6b4c8229ff7036
catspie92@gmail.com <catspie92@gmail.com>
2024-02-18 21:14:43 +0900

    RestControllerAdvice를 활용한 익셉션 핸들러 적용

--- src/main/kotlin/com/example/study/blog/service/BlogService.kt
+++ src/main/kotlin/com/example/study/blog/service/BlogService.kt
@@ -1,6 +1,7 @@
 package com.example.study.blog.service
 
 import com.example.study.blog.dto.BlogDto
+import com.example.study.core.exception.InvalidInputException
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
@@ -15,6 +16,23 @@ class BlogService {
     var restApiKey: String = "97aa17ffd2263381e8bdf880b7de3958"
 
     fun searchKakao(blogDto: BlogDto): String? {
+
+        val msgList = mutableListOf<ExceptionMsg>()
+
+        if(blogDto.query.trim().isEmpty()){
+            msgList.add(ExceptionMsg.EMPTY_QUERY)
+        }
+        if(blogDto.sort.trim() !in arrayOf("accuracy", "recency")){
+            msgList.add(ExceptionMsg.NOT_IN_SORT)
+        }
+        when{
+            blogDto.page < 1 -> msgList.add(ExceptionMsg.LESS_THAN_MIN)
+            blogDto.page > 50 -> msgList.add(ExceptionMsg.MORE_THAN_MAX)
+        }
+        if(msgList.isNotEmpty()){
+            val message = msgList.joinToString { it.msg }
+            throw InvalidInputException(message)
+        }
         println(blogDto.toString())
         val webClient = WebClient
             .builder()
@@ -40,4 +58,11 @@ class BlogService {
 
         return "SearchKakao";
     }
+}
+
+private enum class ExceptionMsg(val msg: String){
+    EMPTY_QUERY("query parameter required"),
+    NOT_IN_SORT("sort parameter one of accuracy and recency"),
+    LESS_THAN_MIN("page is less than min"),
+    MORE_THAN_MAX("page is more than max")
 }
